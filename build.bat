cc55a8451795d9dd3b663e3c7a3784b3947e671a
Martin <mdk@amescon.com>
2014-05-05 16:56:25 +0200

    build.bat creates source package per default

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null *.install PKGBUILD pi@192.168.2.107:/home/pi/raspicommrs485
 
 :: update the checksums & build the package
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null pi@192.168.2.107 "cd /home/pi/raspicommrs485; updpkgsums; makepkg -f"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null pi@192.168.2.107 "cd /home/pi/raspicommrs485; updpkgsums; makepkg -f --source"
